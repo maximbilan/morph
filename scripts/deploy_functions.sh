a93d27debb42bfd4b89aed11e18c43c0d7556ffb
Maksym Bilan <maximb.mail@gmail.com>
2025-04-21 14:32:26 +0300

    deploy script + action

---
+++ scripts/deploy_functions.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+HANDLER_FUNC_NAME="handler"
+
+# Set the runtime
+RUNTIME="go122"
+# Set the project ID
+PROJECT_ID=$MORPH_PROJECT_ID
+
+# Set environment variables
+ENV_PARAMS=("MORPH_PROJECT_ID=$MORPH_PROJECT_ID" "MORPH_SERVER_REGION=$MORPH_SERVER_REGION" "CLOUD=true")
+ENV_VARS=""
+for PARAM in "${ENV_PARAMS[@]}"; do
+  ENV_VARS+="$PARAM,"
+done
+ENV_VARS=${ENV_VARS%,}
+
+# Set the secret environment variables
+SECRET_PARAMS=("MORPH_TELEGRAM_BOT_TOKEN=telegram_bot_token")
+SECRETS=""
+for PARAM in "${SECRET_PARAMS[@]}"; do
+  SECRETS+="$PARAM:latest,"
+done
+SECRETS=${SECRETS%,}
+
+# Set memory parameter
+MEMORY="256MB"
+
+# Timeout
+SCHEDULER_TIMEOUT=60
+
+# Deploy the handler function
+gcloud functions deploy $HANDLER_FUNC_NAME \
+    --runtime $RUNTIME \
+    --trigger-http \
+    --allow-unauthenticated \
+    --entry-point $HANDLER_FUNC_NAME \
+    --project $PROJECT_ID \
+    --gen2 \
+    --region $CAPY_SERVER_REGION \
+    --set-env-vars $ENV_VARS \
+    --set-secrets $SECRETS \
+    --memory $MEMORY
+
+# Print the deployment status
+if [ $? -eq 0 ]; then
+    echo "Function $HANDLER_FUNC_NAME deployed successfully."
+else
+    echo "Failed to deploy function $HANDLER_FUNC_NAME."
+fi
