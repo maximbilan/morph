568a6cbdc72582e9fdde58ddf7cbfc03320a84d9
Maksym Bilan <maximb.mail@gmail.com>
2025-04-29 19:19:38 +0300

    Code review

--- internal/app/app.go
+++ internal/app/app.go
@@ -27,7 +27,8 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	if message.Text != "" {
 		ctx := context.Background()
 
-		systemPrompt := "You're a data analyst. You have to classify the input into categories and subcategories. The input is a free text. The output should be in JSON format with fields: category, subcategory, amount. The category and subcategory are strings. The amount is a float. The input usually is in Ukrainian language. For example, the input is: '400 Вокал'. The output should be like this: {\"category\": \"Children\", \"subcategory\": \"Vocal\", \"amount\": 400.0}. Here is the JSON of categories and subcategories: " + category.GetCategoriesInJSON()
+		categories := category.GetCategoriesInJSON()
+		systemPrompt := "You're a data analyst. You have to classify the input into categories and subcategories. The input is a free text. The output should be in JSON format with fields: category, subcategory, amount. The category and subcategory are strings. The amount is a float. The input usually is in Ukrainian language. For example, the input is: '400 Вокал'. The output should be like this: {\"category\": \"Children\", \"subcategory\": \"Vocal\", \"amount\": 400.0}. Here is the JSON of categories and subcategories: " + categories
 		userPrompt := "The input is: " + message.Text
 
 		response := aiService.Request("Morph", "Translares free input into: Category, Subcategory, Amount", systemPrompt, userPrompt, &ctx)
