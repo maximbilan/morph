c050793dab2714e805849ecfe71b4bbfbdddf093
Maksym Bilan <maximb.mail@gmail.com>
2025-05-03 11:15:59 +0300

    prompt polish

--- internal/app/app.go
+++ internal/app/app.go
@@ -33,7 +33,8 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 		ctx := context.Background()
 
 		categories := category.GetCategoriesInJSON()
-		systemPrompt := "You're a data analyst. You have to classify the input into categories and subcategories. The input is a free text. The output should be in JSON format with fields: category, subcategory, amount. The category and subcategory are strings. The amount is a float. The input usually is in Ukrainian language. For example, the input is: '400 Вокал'. The output should be like this: {\"category\": \"Children\", \"subcategory\": \"Vocal\", \"amount\": 400.0}. Here is the JSON of categories and subcategories: " + categories
+		hints := category.GetHintsInJSON()
+		systemPrompt := "You're a data analyst. You have to classify the input into categories and subcategories. The input is a free text. The output should be in JSON format with fields: category, subcategory, amount. The category and subcategory are strings. The amount is a float. The input usually is in Ukrainian language. If you can't find any proper categories, it should go to the Other category with no subcategory. For example, the input is: '400 Вокал'. The output should be like this: {\"category\": \"Children\", \"subcategory\": \"Vocal\", \"amount\": 400.0}. Here is the JSON of categories and subcategories: " + categories + "Also, here are some hints for categories: " + hints
 		userPrompt := "The input is: " + message.Text
 
 		response := aiService.Request("Morph", "Translares free input into: Category, Subcategory, Amount", systemPrompt, userPrompt, &ctx)
