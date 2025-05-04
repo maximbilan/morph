7075204a587fe098614c46cd69e7ec1e37db9bbc
Maksym Bilan <maximb.mail@gmail.com>
2025-05-04 12:00:22 +0300

    Task service fixes (#33)

--- cmd/main.go
+++ cmd/main.go
@@ -14,7 +14,10 @@ func main() {
 	log.Println("Starting morph on port", port)
 
 	http.HandleFunc("/cashHandler", app.CashHandler)
+	http.HandleFunc("/monoHandler", app.MonoHandler)
 	http.HandleFunc("/monoWebHook", app.MonoWebHook)
+	http.HandleFunc("/sendMessage", app.SendMessage)
+
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 		log.Fatalf("Error starting server: %s", err)
 	}
