a8dbfe89fc8b4ca58a0e3e10b6ebbc77fcee8bc8
Maksym Bilan <maximb.mail@gmail.com>
2025-05-04 10:17:14 +0300

    replyTo param

--- internal/taskservice/scheduled_message.go
+++ internal/taskservice/scheduled_message.go
@@ -1,6 +1,7 @@
 package taskservice
 
 type ScheduledMessage struct {
-	ChatID int64  `json:"chatId"`
-	Text   string `json:"text"`
+	ChatID           int64  `json:"chatId"`
+	Text             string `json:"text"`
+	ReplyToMessageID *int64 `json:"reply_to_message_id,omitempty"`
 }
