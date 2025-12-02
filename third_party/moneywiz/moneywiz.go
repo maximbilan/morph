93f1eb35ebead832d92a0eefdd19e36e1583f60e
Maksym Bilan <maximb.mail@gmail.com>
2025-12-02 18:08:15 +0200

    Transaction time

--- third_party/moneywiz/moneywiz.go
+++ third_party/moneywiz/moneywiz.go
@@ -2,6 +2,7 @@ package moneywiz
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
@@ -15,7 +16,10 @@ func (g DeepLinkGenerator) Create(category string, subcategory string, account s
 		finalizedCategory += "/" + subcategory
 	}
 
-	formattedDate := date.Format("2006-01-02")
+	// MoneyWiz expects date in format: yyyy-MM-dd HH:mm:ss
+	formattedDate := date.Format("2006-01-02 15:04:05")
+	// Encode the space as %20 to keep the URL valid while preserving ":" characters.
+	formattedDate = strings.ReplaceAll(formattedDate, " ", "%20")
 
 	return fmt.Sprintf(
 		"moneywiz://expense?amount=%.2f&account=%s&category=%s&date=%s&save=true",
